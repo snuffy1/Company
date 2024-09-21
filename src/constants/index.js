dffc2572580ac03d08fef1979b2daa46a11bfd9d
snuffy1 <aayushmalla62@gmail.com>
2024-09-21 21:29:38 +0545

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,259 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Sabin Lamichhane',
+    position: 'Youth Engagement Specialist at Food and Agriculture Organization ',
+    img: 'assets/w.jpg',
+    review:
+      'Working with Citizens Technology was a fantastic experience. They transformed our outdated website into a modern, user-friendly platform. Their attention to detail and commitment to quality are unmatched. Highly recommend them for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Rajan Koirala',
+    position: 'Founder of Mindset Cube',
+    img: 'assets/revi.jpg',
+    review:
+      'Citizens Technology expertise in web development is truly impressive. They delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. They are true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'Manita Raj Karnikar',
+    position: 'Senior Consultant At Star Hospital',
+    img: 'assets/dr.jpg',
+    review:
+      'I can’t say enough good things about Citizens Technology. They were able to take our complex project requirements and turn them into a seamless, functional website. Their problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Yogendra Shakya',
+    position: 'Chairman Of Ambassador Hotel, Club Himalayan',
+    img: 'assets/review4.jpg',
+    review:
+      'Citizens Technology was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. Their skills in both frontend backend dev are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'Mallafy - Movie Info',
+    desc: 'A movie typically includes details about its plot, cast, director, release date, genre, and production, offering insights into its themes, visual style, and audience reception.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Movify is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/logo.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+  {
+    title: 'PortFolio - Developer Portfolio',
+    desc: 'Im a passionate and dedicated front-end developer with a strong focus on creating dynamic, responsive, and user-friendly web applications. With a solid foundation in React and JavaScript, I thrive on building clean, maintainable code that delivers exceptional user experiences. Over the past few years, I’ve had the opportunity to work on various projects, ranging from single-page applications to complex e-commerce platforms. My expertise in modern web technologies like React, TypeScript, and CSS allows me to create visually appealing and highly functional applications.',
+    subdesc:
+      'In addition to my technical skills, I’m always eager to learn and stay updated on the latest trends and tools in web development. Whether it’s exploring new frameworks, optimizing performance, or enhancing accessibility, I believe in continuous improvement and bringing the best to every project. I’m excited to collaborate on innovative projects that challenge me and help drive impactful solutions.',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+  {
+    title: 'Star Hospital - Health Management System',
+    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    subdesc:
+      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+  {
+    title: 'Global Peace Foundation',
+    desc: 'The Global Peace Foundation (GPF) is an international nonprofit organization founded in 2009, promoting peacebuilding through a values-based approach. Guided by the vision of "One Family under God," GPF engages communities, governments, and organizations worldwide to foster social cohesion and address conflicts based on universal principles and shared values',
+    subdesc:
+      'GPFs initiatives span peacebuilding, education, youth leadership development, womens leadership, and more. They operate in over 20 countries, hosting events and grassroots projects aimed at sustainable development and global peace.',
+    texture: '/textures/project/project0.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+  {
+    title: 'Restro - Taste and Best',
+    desc: ' Our passion for food is matched only by our commitment to sourcing the finest, locally-sourced ingredients. From our signature dishes to our curated drink selection, every meal is crafted with love and attention to detail. ',
+    subdesc:
+      'Whether you are joining us for a casual lunch, a family dinner, or a special occasion, we aim to provide a warm and welcoming atmosphere where exceptional flavors meet impeccable service.',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/res.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Framer',
+    pos: 'Lead Web Developer',
+    duration: '2022 - Present',
+    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
+    icon: '/assets/framer.svg',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Figma',
+    pos: 'Web Developer',
+    duration: '2020 - 2022',
+    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
+    icon: '/assets/figma.svg',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Notion',
+    pos: 'Junior Web Developer',
+    duration: '2019 - 2020',
+    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
+    icon: '/assets/notion.svg',
+    animation: 'salute',
+  },
+];
